ced73b03813ea33d53bc6dda3657189793f6a776
treedel <devesh.k1203@gmail.com>
2026-02-12 22:13:53 +0530

    Added working files

---
+++ src/robot_navigation/setup.py
@@ -0,0 +1,34 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'robot_navigation'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
+        (os.path.join('share', package_name, 'maps'), glob('maps/*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='devesh',
+    maintainer_email='devesh@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
